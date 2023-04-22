baabe9f3e1618d11646c6233ad5ba72034f0ef77
Matheus Evers <matheuseversrfernandes@gmail.com>
2023-04-22 01:21:20 -0300

    getting dolar from API and posting to slack

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-var CronJob = require('cron').CronJob;
-var job = new CronJob(
-    '* 1 * * * *',
-    function() {
-        console.log('You will see this message every minute');
+const CronJob = require('cron').CronJob;
+const axios = require('axios');
+require('dotenv').config();
+
+const getDolarValue = async () => {
+    const cotacao = await axios.get("https://economia.awesomeapi.com.br/last/USD-BRL");
+      
+      if (cotacao.status === 200) {
+        console.log('Sending slack message!');
+        await axios.post('https://slack.com/api/chat.postMessage',{
+            "channel": process.env.SLACK_CHANNEL_ID,
+            "text": `A cotação ${cotacao.data.USDBRL.name} hoje é de: ${cotacao.data.USDBRL.high} USD`
+        }, {
+            headers: {
+                "Content-type": "application/json",
+                Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
+            }
+        })
+
+      }
+}
+
+const job = new CronJob(
+    '0 */1 * * * *',
+    async function() {
+        getDolarValue()
     },
     null,
     false,
@@ -10,3 +31,4 @@ var job = new CronJob(
 );
 // Use this if the 4th param is default value(false)
 job.start()
+
