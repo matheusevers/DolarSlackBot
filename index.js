f00a7c773da937261ed14e792e30db6dd491f0de
Matheus Evers <matheuseversrfernandes@gmail.com>
2023-04-22 00:36:19 -0300

    changing to 1 minute

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var CronJob = require('cron').CronJob;
 var job = new CronJob(
-    '* * * * * *',
+    '* 1 * * * *',
     function() {
         console.log('You will see this message every second');
     },
