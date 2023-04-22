fe078346143fc4c24d774f7cf428f86b85ec4b22
Matheus Evers <matheuseversrfernandes@gmail.com>
2023-04-22 11:05:32 -0300

    adding a response

--- index.js
+++ index.js
@@ -9,22 +9,25 @@ const app = express();
 const port = 3000;
 
 
-const getDolarValue = async () => {
+const getDolarValue = async (res) => {
     try {
         const cotacao = await axios.get("https://economia.awesomeapi.com.br/last/USD-BRL");
+        console.log(parseFloat(cotacao.data.USDBRL.high).toFixed(2))
       
         if (cotacao.status === 200) {
             console.log('Sending slack message!');
             try {
                 await axios.post('https://slack.com/api/chat.postMessage',{
                     "channel": process.env.SLACK_CHANNEL_ID,
-                    "text": `A cotação ${cotacao.data.USDBRL.name} hoje é de: ${cotacao.data.USDBRL.high} USD`
+                    "text": `A cotação ${cotacao.data.USDBRL.name} hoje é de: ${parseFloat(cotacao.data.USDBRL.high).toFixed(2)} USD`
                 }, {
                     headers: {
                         "Content-type": "application/json",
                         Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
                     }
                 })
+
+                res.send('success')
             } catch (error) {
                 console.log('Error to post slack message: ', error)
             }
@@ -36,7 +39,9 @@ const getDolarValue = async () => {
 
 app.get('/', (req, res) => {
     console.log('Running job...')
-    getDolarValue()
+    
+    getDolarValue(res)
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
