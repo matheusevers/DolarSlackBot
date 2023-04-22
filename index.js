b31af78450f0c4f1ffdc65a411fa965429c68c34
Matheus Evers <matheuseversrfernandes@gmail.com>
2023-04-22 01:24:05 -0300

    Adding try catch

--- index.js
+++ index.js
@@ -3,21 +3,28 @@ const axios = require('axios');
 require('dotenv').config();
 
 const getDolarValue = async () => {
-    const cotacao = await axios.get("https://economia.awesomeapi.com.br/last/USD-BRL");
+    try {
+        const cotacao = await axios.get("https://economia.awesomeapi.com.br/last/USD-BRL");
       
-      if (cotacao.status === 200) {
-        console.log('Sending slack message!');
-        await axios.post('https://slack.com/api/chat.postMessage',{
-            "channel": process.env.SLACK_CHANNEL_ID,
-            "text": `A cotação ${cotacao.data.USDBRL.name} hoje é de: ${cotacao.data.USDBRL.high} USD`
-        }, {
-            headers: {
-                "Content-type": "application/json",
-                Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
+        if (cotacao.status === 200) {
+            console.log('Sending slack message!');
+            try {
+                await axios.post('https://slack.com/api/chat.postMessage',{
+                    "channel": process.env.SLACK_CHANNEL_ID,
+                    "text": `A cotação ${cotacao.data.USDBRL.name} hoje é de: ${cotacao.data.USDBRL.high} USD`
+                }, {
+                    headers: {
+                        "Content-type": "application/json",
+                        Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
+                    }
+                })
+            } catch (error) {
+                console.log('Error to post slack message: ', error)
             }
-        })
-
-      }
+        }
+    } catch (error) {
+        console.log('Error to get dolar value: ', error)
+    }
 }
 
 const job = new CronJob(
